7e717165fe07c38781bccef7a6fe3022a861b055
MrPekar98 <martin.pekar@outlook.dk>
2018-05-14 16:30:59 +0200

    Added simulation of a communication. Node simulator still missing.

--- A306/Communication/Client.java
+++ A306/Communication/Client.java
@@ -116,7 +116,7 @@ public class Client
 
         // Entering address.
         console.print("Address (format: xxx-xxx): ");
-        keyPair = receiveKey(input);
+        keyPair = CommunicationSimulator.receiveKey(input);
 
         while (true)
         {
@@ -140,107 +140,5 @@ public class Client
                 break;
             }
         }
-    }
-
-    // Entering receivers public key as address.
-    private static int[] receiveKey(BufferedReader reader)
-    {
-        String key;
-
-        try
-        {
-            key = reader.readLine();
-
-            // Checking validity of entered key pair.
-            if (!isKeyValid(key))
-                throw new IOException();
-
-            else
-            {
-                return new int[]{Integer.parseInt(getNumber(key, 0, '-')), Integer.parseInt(getNumber(key, '-'))};
-            }
-        }
-
-        catch (IOException e)
-        {
-            return new int[]{0, 0};
-        }
-    }
-
-    // Checks if entered key pair is valid.
-    private static boolean isKeyValid(String key)
-    {
-        char[] keyArray = key.toCharArray();
-        int amountOfDash = 0;
-
-        if (keyArray[0] == '-' || keyArray[keyArray.length - 1] == '-')
-            return false;
-
-        // Checking all characters.
-        for (int i = 0; i < keyArray.length; i++)
-        {
-            if (((int) keyArray[i] < 48 || (int) keyArray[i] > 57) && keyArray[i] != '-')
-                return false;
-
-            if (keyArray[i] == '-')
-                amountOfDash++;
-
-            if (amountOfDash > 1)
-                return false;
-        }
-
-        if (amountOfDash == 1)
-            return true;
-
-        else
-            return false;
-    }
-
-    // Gets a number from a String.
-    private static String getNumber(String text, char startIndex)
-    {
-        char[] number = text.toCharArray();
-        String returnNumber = "";
-        boolean indexFound = false;
-        int j = 0;
-
-        for (int i = 0; i < number.length; i++)
-        {
-            if (number[i] == startIndex)
-            {
-                indexFound = true;
-                continue;
-            }
-
-            if (indexFound)
-            {
-                returnNumber = returnNumber + number[i];
-                j++;
-            }
-        }
-
-        if (returnNumber.length() > 0)
-            return returnNumber;
-
-        else
-            return "0";
-    }
-
-    // Copy of previous getNumber, but using int as index.
-    private static String getNumber(String text, int startIndex, char endIndex)
-    {
-        char[] number = text.toCharArray();
-        String returnNumber = "";
-
-        for (int i = startIndex; i < number.length && number[i] != endIndex; i++)
-        {
-            returnNumber = returnNumber + number[i];
-        }
-
-        if (returnNumber.length() > 0)
-            return returnNumber;
-
-        else
-            return "0";
     }
 }
--- A306/Communication/CommunicationSimulator.java
+++ A306/Communication/CommunicationSimulator.java
@@ -1,27 +1,163 @@
 package Communication;
 
+import RSA.KeyPairGenerator;
+import RSA.RSAOAEPDecrypt;
+import RSA.RSAOAEPEncrypt;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Random;
 
 public class CommunicationSimulator
 {
+    // Entering receivers public key as address.
+    public static int[] receiveKey(BufferedReader reader)
+    {
+        String key;
+
+        try
+        {
+            key = reader.readLine();
+
+            // Checking validity of entered key pair.
+            if (!isKeyValid(key))
+                throw new IOException();
+
+            else
+            {
+                return new int[]{Integer.parseInt(getNumber(key, 0, '-')), Integer.parseInt(getNumber(key, '-'))};
+            }
+        }
+
+        catch (IOException e)
+        {
+            return new int[]{0, 0};
+        }
+    }
+
+    // Checks if entered key pair is valid.
+    public static boolean isKeyValid(String key)
+    {
+        char[] keyArray = key.toCharArray();
+        int amountOfDash = 0;
+
+        if (keyArray[0] == '-' || keyArray[keyArray.length - 1] == '-')
+            return false;
+
+        // Checking all characters.
+        for (int i = 0; i < keyArray.length; i++)
+        {
+            if (((int) keyArray[i] < 48 || (int) keyArray[i] > 57) && keyArray[i] != '-')
+                return false;
+
+            if (keyArray[i] == '-')
+                amountOfDash++;
+
+            if (amountOfDash > 1)
+                return false;
+        }
+
+        if (amountOfDash == 1)
+            return true;
+
+        else
+            return false;
+    }
+
+    // Gets a number from a String.
+    private static String getNumber(String text, char startIndex)
+    {
+        char[] number = text.toCharArray();
+        String returnNumber = "";
+        boolean indexFound = false;
+        int j = 0;
+
+        for (int i = 0; i < number.length; i++)
+        {
+            if (number[i] == startIndex)
+            {
+                indexFound = true;
+                continue;
+            }
+
+            if (indexFound)
+            {
+                returnNumber = returnNumber + number[i];
+                j++;
+            }
+        }
+
+        if (returnNumber.length() > 0)
+            return returnNumber;
+
+        else
+            return "0";
+    }
+
+    // Copy of previous getNumber, but using int as index.
+    private static String getNumber(String text, int startIndex, char endIndex)
+    {
+        char[] number = text.toCharArray();
+        String returnNumber = "";
+
+        for (int i = startIndex; i < number.length && number[i] != endIndex; i++)
+        {
+            returnNumber = returnNumber + number[i];
+        }
+
+        if (returnNumber.length() > 0)
+            return returnNumber;
+
+        else
+            return "0";
+    }
+
+    // Main method
     public static void main(String[] args)
     {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        simulateCommunication();
+    }
 
-        System.out.print("Message: ");
+    // Simulates a client-node communication.
+    public static void simulateCommunication()
+    {
+        KeyPairGenerator keys = new KeyPairGenerator(2048);
+
+        clientSimulator(keys);
+
+        // Add nodeSimulator.
+    }
+
+    private static void clientSimulator(KeyPairGenerator keys)
+    {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger[] key = new BigInteger[]{keys.getPublicKey(), keys.getPublicE()};
+
+        System.out.println("Public key of receiver:\nn: " + key[0] + "\ne : " + key[1]);
+        System.out.print("\nMessage: ");
 
         try
         {
             String message = input.readLine();
-            System.out.println("Message was \"" + message + "\".");
+
+            // Start encryption.
+            RSAOAEPEncrypt encrypt = new RSAOAEPEncrypt(message, new byte[]{1, 2}, key[0], key[1]);
+            System.out.println("\nEncrypted message: " + Arrays.toString(encrypt.getEncryptedMessage()));
         }
 
         catch (IOException e)
         {
             System.out.println(e.getCause());
         }
+    }
+
+    public static String showDecrypted(byte[] encrypted)
+    {
+        return new String(encrypted);
     }
 }
