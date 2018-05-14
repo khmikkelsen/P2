13764f6dc38b9bb9a98b51f74eabaa8e43d1cd8f
Melanie Selman <melanieselman@hotmail.com>
2018-05-14 14:46:51 +0200

    TargetTest

---
+++ test/robin/TargetTest.java
@@ -0,0 +1,81 @@
+package robin;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// lav færdigt efter møde med Mads
+
+class TargetTest {
+
+
+
+    @Test
+    void calculateCompactTargetTest01() {
+
+        BigInteger num = new BigInteger("1000", 10);
+
+        String compactTarget = Target.calculateCompactTarget(num);
+
+        assertEquals("0203e800", compactTarget);
+
+
+    }
+
+    @Test
+    void calculateCompactTargetTest02() {
+
+        BigInteger num = new BigInteger("1000", 10);
+
+        String compactTarget = Target.calculateCompactTarget(num);
+
+
+        assertNotEquals("0203e800h", Target.calculateCompactTarget(num));
+
+    }
+
+
+    @Test
+    void calculateCompactTargetTest03() {
+
+        BigInteger num = new BigInteger("ffffffffffffffffffffffffffffffffffffffffffffffffffffffff",16);
+
+        String compactTarget = Target.calculateCompactTarget(num);
+
+        assertEquals("1d00ffff", Target.calculateCompactTarget(num));
+
+
+    }
+
+    @Test
+    void calculateCompactTargetTest04() {
+
+        BigInteger num = new BigInteger("65535",10);
+
+        String compactTarget = Target.calculateCompactTarget(num);
+
+        assertEquals("0300ffff", Target.calculateCompactTarget(num));
+
+
+    }
+
+
+
+
+    @Test
+    void calculateBigIntegerTarget01() {
+
+        String compactTarget = "180130E0";
+
+        BigInteger num1 = new BigInteger("130E0000000000000000000000000000000000000000000", 16);
+
+        BigInteger num2 = Target.calculateBigIntergerTarget(compactTarget);
+
+        assertEquals(num1, num2);
+
+
+
+    }
+}
