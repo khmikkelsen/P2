1253baec98fc882085381175ba613d32154dc49d
Robin Nørlev Ezra <robin@setio.run>
2018-05-17 15:02:30 +0200

    Changed KeyPairGenerator to create RSAKeys from constructor

--- A306/Communication/CommunicationSimulator.java
+++ A306/Communication/CommunicationSimulator.java
@@ -11,36 +11,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class CommunicationSimulator
-{
+public class CommunicationSimulator {
     // Entering receivers public key as address.
-    public static int[] receiveKey(BufferedReader reader)
-    {
+    public static int[] receiveKey(BufferedReader reader) {
         String key;
 
-        try
-        {
+        try {
             key = reader.readLine();
 
             // Checking validity of entered key pair.
             if (!isKeyValid(key))
                 throw new IOException();
 
-            else
-            {
+            else {
                 return new int[]{Integer.parseInt(getNumber(key, 0, '-')), Integer.parseInt(getNumber(key, '-'))};
             }
-        }
-
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             return new int[]{0, 0};
         }
     }
 
     // Checks if entered key pair is valid.
-    public static boolean isKeyValid(String key)
-    {
+    public static boolean isKeyValid(String key) {
         char[] keyArray = key.toCharArray();
         int amountOfDash = 0;
 
@@ -48,8 +40,7 @@ public class CommunicationSimulator
             return false;
 
         // Checking all characters.
-        for (int i = 0; i < keyArray.length; i++)
-        {
+        for (int i = 0; i < keyArray.length; i++) {
             if (((int) keyArray[i] < 48 || (int) keyArray[i] > 57) && keyArray[i] != '-')
                 return false;
 
@@ -68,23 +59,19 @@ public class CommunicationSimulator
     }
 
     // Gets a number from a String.
-    private static String getNumber(String text, char startIndex)
-    {
+    private static String getNumber(String text, char startIndex) {
         char[] number = text.toCharArray();
         String returnNumber = "";
         boolean indexFound = false;
         int j = 0;
 
-        for (int i = 0; i < number.length; i++)
-        {
-            if (number[i] == startIndex)
-            {
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] == startIndex) {
                 indexFound = true;
                 continue;
             }
 
-            if (indexFound)
-            {
+            if (indexFound) {
                 returnNumber = returnNumber + number[i];
                 j++;
             }
@@ -98,13 +85,11 @@ public class CommunicationSimulator
     }
 
     // Copy of previous getNumber, but using int as index.
-    private static String getNumber(String text, int startIndex, char endIndex)
-    {
+    private static String getNumber(String text, int startIndex, char endIndex) {
         char[] number = text.toCharArray();
         String returnNumber = "";
 
-        for (int i = startIndex; i < number.length && number[i] != endIndex; i++)
-        {
+        for (int i = startIndex; i < number.length && number[i] != endIndex; i++) {
             returnNumber = returnNumber + number[i];
         }
 
@@ -116,25 +101,27 @@ public class CommunicationSimulator
     }
 
     // Main method
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         simulateCommunication();
     }
 
     // Simulates a client-node communication.
-    public static void simulateCommunication()
-    {
-        KeyPairGenerator keysReceiver = new KeyPairGenerator(2048);
-        KeyPairGenerator keysSender = new KeyPairGenerator(2048);
+    public static void simulateCommunication() {
+        try {
+            KeyPairGenerator keysReceiver = new KeyPairGenerator(2048);
+            KeyPairGenerator keysSender = new KeyPairGenerator(2048);
 
-        String encryptedMessage = clientSimulator(keysReceiver, keysSender);
+            String encryptedMessage = clientSimulator(keysReceiver, keysSender);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
 
         // Add nodeSimulator.
     }
 
     // Simulates a client and return encrypted message.
-    private static String clientSimulator(KeyPairGenerator receiverKeys, KeyPairGenerator senderKeys)
-    {
+    private static String clientSimulator(KeyPairGenerator receiverKeys, KeyPairGenerator senderKeys) {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
         // Sender keys
@@ -144,25 +131,19 @@ public class CommunicationSimulator
         System.out.println("Public key of receiver:\nn: " + receiverKeys.getPublicKey().getModulus() + "\ne : " + receiverKeys.getPublicKey().getExponent());
         System.out.print("\nMessage: ");
 
-        try
-        {
+        try {
             String message = input.readLine();
 
             // Start preparing message to be send.
             return prepareMessage(message, receiverKeys, senderKeys);
-        }
-
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             return e.getCause().toString();
         }
     }
 
     // Prepares message to be send to node.
-    private static String prepareMessage(String message, KeyPairGenerator receiverKeys, KeyPairGenerator senderKeys)
-    {
-        try
-        {
+    private static String prepareMessage(String message, KeyPairGenerator receiverKeys, KeyPairGenerator senderKeys) {
+        try {
             // Encrypting message.
             RSAOAEPEncrypt encrypt = new RSAOAEPEncrypt(message, new byte[]{1, 2}, receiverKeys.getPublicKey());
             System.out.println("\nEncrypted message: " + showEncrypted(encrypt.getEncryptedMessage()) + "\n");
@@ -181,23 +162,18 @@ public class CommunicationSimulator
             System.out.println(preparedMessage);
 
             return preparedMessage;
-        }
-
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             return "IOException.";
         }
     }
 
     // Returns a String of encrypted message.
-    private static String showEncrypted(byte[] encrypted)
-    {
+    private static String showEncrypted(byte[] encrypted) {
         return Arrays.toString(encrypted);
     }
 
     // Returns a String of decrypted message.
-    private static String showDecrypted(byte[] decrypted)
-    {
+    private static String showDecrypted(byte[] decrypted) {
         return new String(decrypted);
     }
 }
--- A306/RSA/KeyPairGenerator.java
+++ A306/RSA/KeyPairGenerator.java
@@ -1,45 +1,44 @@
 package RSA;
 
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
-public class KeyPairGenerator extends RSAOAEP
-{
-    private BigInteger publicKey; // Public key eksponent
-    private BigInteger privateKey; // Private key eksponent
-    private BigInteger rsaMod;
+public class KeyPairGenerator extends RSAOAEP {
+
+    private RSAKey privateKey;
+    private RSAKey publicKey;
+
 
     // Constructor
-    public KeyPairGenerator (int keyBitSize)
-    {
-        this.publicKey = new BigInteger("65537");
-        genKeys(keyBitSize/2);
+    public KeyPairGenerator(int keyBitSize) throws IOException {
+        genKeys(keyBitSize / 2, new BigInteger("65537"));
     }
-    private void genKeys(int keyBitsSize)
-    {
-        boolean check = false;
+
+    private void genKeys(int keyBitsSize, BigInteger publicExponent) throws IOException {
+
         BigInteger p, q, lambda;
         SecureRandom rand = new SecureRandom();
 
-        while (!check)
-        {
+        while (true) {
             p = BigInteger.probablePrime(keyBitsSize, rand);
             q = BigInteger.probablePrime(keyBitsSize, rand);
-            this.rsaMod = p.multiply(q);
+            BigInteger rsaMod = p.multiply(q);
 
             if (rsaMod.bitLength() % 8 == 0) {
                 lambda = lcm(p.subtract(BigInteger.ONE), q.subtract(BigInteger.ONE));
-                this.privateKey = publicKey.modInverse(lambda);
-                check = true;
+                BigInteger privateExponent = publicExponent.modInverse(lambda);
+
+                this.publicKey = new RSAKey(rsaMod, publicExponent);
+                this.privateKey = new RSAKey(rsaMod, privateExponent);
+
+                break;
             }
         }
-
     }
 
-
     // lcm (least common multiple) ved reduktion af greatest common divisor (gcd).
-    private BigInteger lcm(BigInteger a, BigInteger b)
-    {
+    private BigInteger lcm(BigInteger a, BigInteger b) {
         BigInteger AGcdB = a.gcd(b);
         BigInteger ATimesB = a.multiply(b);
 
@@ -47,9 +46,13 @@ public class KeyPairGenerator extends RSAOAEP
     }
 
     // Getters
-    public RSAKey getPublicKey() { return new RSAKey(rsaMod, publicKey); }
-    public RSAKey getPrivateKey() { return new RSAKey(rsaMod, privateKey); }
+    public RSAKey getPublicKey() {
+        return publicKey;
+    }
 
+    public RSAKey getPrivateKey() {
+        return privateKey;
+    }
 
 
 }
