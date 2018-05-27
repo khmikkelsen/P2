2880796b3f0790a78b9364b8c3f2b662dc40444a
Robin Nørlev Ezra <robin@setio.run>
2018-05-27 10:41:53 +0200

    Moved merkle root calculations to BlockUtil

--- A306/robin/Block.java
+++ A306/robin/Block.java
@@ -54,7 +54,7 @@ public class Block {
 
     public void mineBlock() {
 
-        this.merkleRootHash = getMerkleRootHash();
+        this.merkleRootHash = BlockUtil.calculateMerkleRootHash(messages);
 
         while (new BigInteger(this.calculateHash(), 16).compareTo(Chain.getTarget().getBigIntegerTarget()) > 0) {
             if (nonce == Integer.MAX_VALUE) {
@@ -66,61 +66,9 @@ public class Block {
         }
     }
 
-    /**
-     * The method calcMerkleHash: takes each message(Blockchain transaction) and creates a hash of it, and then
-     * uses them to create the Merkle root hash.
-     *
-     * @return Merkle root, consisting of a blocks transaction hashes
-     */
     public String getMerkleRootHash() {
-        List<String> hashedMessages = new ArrayList<>();
-
-        for (Message m : messages) {
-            hashedMessages.add(m.calculateHash());
-        }
-        // The list of hashes are given to getMerkleRootHash function; a Merkle root is returned.
-        String merkleRootHash = calculateMerkleRootHash(hashedMessages);
-
         return merkleRootHash;
     }
-
-    /**
-     * The function getMerkleRootHash: takes a list of hashes; If there is more than 1 node.
-     * For as long as i is less than amount of hashes, then hash i and i+1 get combined into a new hash and added to the
-     * newNodes list. hashedCount iterates after a combination is made.  i is iterated by 2 beacuse 2 hashes are combined
-     * each time.
-     * When out of for loop, nodeSize is checked against hashedCount to explore if any hashes remains uncombined. If yes,
-     * it is added to the newNodes list.
-     * Is recursive. The whole process starts again, with the now updated hash list : newNodes.
-     * When the newNode list has a size of 1, then the function returns the first hash in list, which is now
-     * a Merkle root.
-     *
-     * @param nodes to generate a Merkle root from
-     * @return nodes, or rather the newly generated merkle root , which is the first and only element in nodes list.
-     */
-    private String calculateMerkleRootHash(List<String> nodes) {
-        if (nodes.size() > 1) {
-            List<String> newNodes = new ArrayList<>();
-
-            int hashedCount = 0;
-
-            for (int i = 0; i < nodes.size() - 1; i += 2) {
-                String combinedHash = StringUtil.applySha256(nodes.get(i) + nodes.get(i + 1));
-                hashedCount += 2;
-                newNodes.add(combinedHash);
-            }
-
-            if (nodes.size() > hashedCount) {
-                newNodes.add(nodes.get(nodes.size() - 1));
-            }
-
-            return calculateMerkleRootHash(newNodes);
-        }
-
-        return nodes.get(0);
-    }
-
-
 
     public int getNonce() {
         return nonce;
