ec2e8fb6ccdce9442ff95bdb0b61f688a7e44092
MrPekar98 <martin.pekar@outlook.dk>
2018-06-11 18:32:19 +0200

    Fixed errors.

--- test/blockchain/utils/StringUtilTest.java
+++ test/blockchain/utils/StringUtilTest.java
@@ -11,21 +11,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilTest {
 
-
     @Test
     void applySha265Test01() {
         String str = "a b c d e f g h i j k l m n o p q r s t u v w x y z æ ø å !\"#¤%&/()=?`";
-
         String hash = "7a36eab637f03c29aa3b0896049534ee949f4a7ac81fdeb99e9a440972139db2";
 
         assertEquals(hash, StringUtil.applySha256(str));
-
     }
 
     @Test
     void applySha265Test02() {
         String str = "b c d e f g h i j k l m n o p q r s t u v w x y z æ ø å !\"#¤%&/()=?`";
-
         String hash = "7a36eab637f03c29aa3b0896049534ee949f4a7ac81fdeb99e9a440972139db2";
 
         assertNotEquals(hash, StringUtil.applySha256(str));
